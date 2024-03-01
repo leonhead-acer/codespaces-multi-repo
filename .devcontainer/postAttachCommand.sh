3b335f9a8a6dd853cc0ffb2a2e6550f67e4658c4
Leon Head <leon.head@acer.org>
2024-03-01 16:39:20 +1100

    Major update - add postAttachCommand.sh, added pgadmin4

--- .devcontainer/postAttachCommand.sh
+++ .devcontainer/postAttachCommand.sh
@@ -1,35 +1,45 @@
 #!/usr/bin/env bash
 
-# script_folder="$(cd $(dirname "${BASH_SOURCE[0]}") && pwd)"
-# workspaces_folder="$(cd "${script_folder}/.." && pwd)"
+script_folder="$(cd $(dirname "${BASH_SOURCE[0]}") && pwd)"
 
-# clone-repo()
-# {
-#     cd "${workspaces_folder}"
-#     if [ ! -d "${1#*/}" ]; then
-#         git clone "https://github.com/$1"
-#     else 
-#         echo "Already cloned $1"
-#     fi
-# }
+clone-repo()
+{
+    cd "${workspaces_folder}"
+    if [ ! -d "${1#*/}" ]; then
+        git clone "https://github.com/$1"
+    else 
+        echo "Already cloned $1"
+    fi
+}
 
-# if [ "${CODESPACES}" = "true" ]; then
-#     # Remove the default credential helper
-#     sudo sed -i -E 's/helper =.*//' /etc/gitconfig
+if [ "${CODESPACES}" = "true" ]; then
+    # Remove the default credential helper
+    sudo sed -i -E 's/helper =.*//' /etc/gitconfig
+    workspaces_folder="$(cd "${HOME}/.." && pwd)"
+    
+else 
+    workspaces_folder="$(cd "${HOME}/workspaces" && pwd)"
+fi
 
-#     # Add one that just uses secrets available in the Codespace
-#     git config --global credential.helper '!f() { sleep 1; echo "username=${GITHUB_USER}"; echo "password=${GH_TOKEN}"; }; f'
-# fi
+if [ -z "${GITHUB_USER}" ]; then
+    git config --global credential.helper '!f() { sleep 1; echo "username=${GITHUB_USER}"; echo "password=${GH_TOKEN}"; }; f'
+fi
 
-# if [ -f ./repos-to-clone.list ]; then
-#     while IFS= read -r repository; do
-#         clone-repo "$repository"
-#     done < ./repos-to-clone.list
-# fi
+if [ -z "${AWS_ACCESS_KEY_ID}" ]; then
+    git config --global credential.helper '!aws codecommit credential-helper $@'
+    git config --global credential.UseHttpPath true
+fi
 
-python3 -m pip install --upgrade pip --root-user-action=ignore;
-python3 -m pip install virtualenv --upgrade-strategy only-if-needed --root-user-action=ignore;
+if [ -f ./repos-to-clone.list ]; then
+    while IFS= read -r repository; do
+        clone-repo "$repository"
+    done < ./repos-to-clone.list
+fi
 
+python3 -m pip install --upgrade pip;
+python3 -m pip install virtualenv --upgrade-strategy only-if-needed;
+
+cd "${workspaces_folder}"
 folders=($(ls -d */));
 
 for f in "${folders[@]}"
@@ -40,6 +50,15 @@ do
     then
         echo "Creating requirements.txt"
         touch ./requirements.txt;
+    fi
+
+    if [ ! -f ./.gitignore ]
+    then
+        echo "Creating .gitignore file"
+        touch ./.gitignore;
+        echo 'renv/' -> ./.gitignore;
+        echo '.venv/' -> ./.gitignore;
+        echo '__pycache__' -> ./.gitignore;
     fi
 
     if [ ! -d "./.venv" ]
@@ -53,10 +72,12 @@ do
 
     source ./.venv/bin/activate;
     echo "Virutal env:" "$VIRTUAL_ENV";
-    python3 -m pip install -r requirements.txt --upgrade-strategy only-if-needed --root-user-action=ignore;
-    python3 -m pip install ipykernel --root-user-action=ignore;
+    python3 -m pip install -r requirements.txt --upgrade-strategy only-if-needed;
+    python3 -m pip install ipykernel;
     python3 -m ipykernel install --user --name ${f%%/};
-    deactivate;
+    source deactivate;
     cd ..;
 
-done
+done;
+
+exec "$@";
